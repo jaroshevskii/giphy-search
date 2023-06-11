bab6cac98803b2988e05ac9ebe4a8606b558e5d7
jaroshevskii <jaroshevskiisasha@gmail.com>
2023-06-12 00:35:33 +0300

    Add infinity scroll

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
+var offset = 0; // Variable to track the offset for retrieving new GIFs.
+
 function searchGIFs() {
   var searchQuery = document.getElementById("searchInput").value;
   var apiKey = "ejiQdEnDapCsfuCg97zET8hR7ZbgEr7B";
-  var url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}`;
+  var url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&offset=${offset}`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
       var gifContainer = document.getElementById("gifContainer");
-      gifContainer.innerHTML = "";
 
       data.data.forEach(gif => {
         var gifImage = document.createElement("img");
         gifImage.src = gif.images.fixed_height.url;
         gifContainer.appendChild(gifImage);
       });
+
+      offset += data.data.length; // Increase the offset by the number of retrieved GIFs.
     })
     .catch(error => console.log(error));
 }
+
+// Scroll event listener.
+window.addEventListener('scroll', function() {
+  // Check if scrolled to the bottom of the page.
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    searchGIFs(); // Call the searchGIFs function when scrolled to the bottom of the page.
+  }
+});
